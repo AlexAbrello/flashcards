6f798a2d4e29eee91195788460da7843ceea098f
AlexAbrello <abrello063@gmail.com>
2024-06-25 21:50:00 +0300

    test commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 
-import "./styles/index.scss";
-
 import { App } from '@/App'
 import { createRoot } from 'react-dom/client'
+
+import './styles/index.scss'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
